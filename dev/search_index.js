52d792ed37d6e1cbfd60a4f7ee93751aa9d372b8
Documenter.jl <documenter@juliadocs.github.io>
2022-09-26 09:17:21 +0000

    build based on 65cf7ce

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = PeriFast","category":"page"},{"location":"#PeriFast","page":"Home","title":"PeriFast","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for PeriFast.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [PeriFast]","category":"page"}]
+}
